870735693bf61cba1139018828d0c531f37ef9dd
Eric Ye <ericye@google.com>
2022-12-24 19:28:21 -0500

    day 17 wip

--- scala/hello-world/src/main/scala/d17.scala
+++ scala/hello-world/src/main/scala/d17.scala
@@ -47,15 +47,18 @@ object d17 extends App {
     rock.rock_shape.map { case (a, b) => (a + offset._1, b + offset._2) }
   }
 
-  def p1(input: String): Int = {
-    var arr = Array.ofDim[Char](5000, 7)
+  def p1(input: String, num_rocks: Int): Int = {
+    var arrHeights = Array.ofDim[Int](7);
+    var arr = Array.ofDim[Char](5000, 7);
     for (w <- 0 until 7; r <- 0 until 5000) {
-      arr(r)(w) = '.';
+      arr(r)(w) = '.'
     };
+    for (w <- 0 until 7) {
+      arrHeights(w) = -1
+    }
     var highest_rock = 0
     var dir_idx = 0;
-    printArray(arr, highest_rock);
-    for (rock_idx: Int <- 0 until 2022) {
+    for (rock_idx: Int <- 0 until num_rocks) {
       val which_rock: Int = rock_idx % rocks.length;
       val rock = rocks(which_rock)
       var rock_origin = (highest_rock + 3, 2)
@@ -77,25 +80,44 @@ object d17 extends App {
         if (
           after_sideways.forall {
             case (h, c) => {
+              // c >= 0 && c < 7 && arrHeights(c) < h
+              val a = c >= 0 && c < 7 && arr(h)(c) == '.'
+              val b = c >= 0 && c < 7 && arrHeights(c) < h
+              if (a != b) {
+                println((h, c))
+                printArray(arr, h + 10)
+                println(arrHeights.mkString(","))
+                throw new RuntimeException("fjdskl")
+              }
               c >= 0 && c < 7 && arr(h)(c) == '.'
             }
           }
         ) {
           rock_origin = orig_after_sideways
         }
+        // println("s " + rock_origin)
         // Down
         val orig_after_down = (rock_origin._1 - 1, rock_origin._2)
         val after_down = moveRock(rock, orig_after_down)
         if (
           after_down.exists {
             case (h, c) => {
-              h < 0 || arr(h)(c) == '#'
+              // if (arrHeights(c) > h) {
+              //   throw new RuntimeException("fdjskl")
+              // }
+              val a = h < 0 || arrHeights(c) >= h
+              val b = h < 0 || arr(h)(c) == '#'
+              if (a != b) {
+                throw new RuntimeException("fdsfdsf 2")
+              }
+              a
             }
           }
         ) {
           // Stop the rock the prior step
           moveRock(rock, rock_origin).foreach {
             case (h, c) => {
+              arrHeights(c) = Math.max(arrHeights(c), h)
               arr(h)(c) = '#'
             }
           }
@@ -103,10 +125,12 @@ object d17 extends App {
           // Took me a while to find this issue
           highest_rock = Math.max(rock_origin._1 + rock.height, highest_rock)
           // printArray(arr, highest_rock + 2)
+          println(arrHeights.mkString(" "))
           // println(highest_rock)
           // println("--------")
         } else {
           rock_origin = orig_after_down;
+          // println("d " + rock_origin)
         }
       }
     }
@@ -114,13 +138,11 @@ object d17 extends App {
     highest_rock
   }
   val filename = args(0);
+  val num_rocks = args(1).toInt
   val input = Using(Source.fromFile(filename)) { source =>
     source.getLines().toArray
   }.get
-  for (rock <- rocks) {
-    println(rock.rock_shape.mkString)
-  }
   for (line <- input) {
-    println(p1(line))
+    println(p1(line, num_rocks))
   }
 }
