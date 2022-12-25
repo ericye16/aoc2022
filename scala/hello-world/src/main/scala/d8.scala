287ad70c5bd3e0b7bb2db1d928e11fbc9e0a5746
Eric Ye <ericye@google.com>
2022-12-24 21:07:00 -0500

    Day 8 part 2

--- scala/hello-world/src/main/scala/d8.scala
+++ scala/hello-world/src/main/scala/d8.scala
@@ -36,10 +36,53 @@ object d8 extends App {
       })
       .sum
   }
+
+  def scenicScore(a: Array[Array[Int]], w: Int, h: Int, r: Int, c: Int): Int = {
+    val v = a(r)(c)
+    val rf = (((r - 1) to 0 by -1)
+      .find(r0 => {
+        a(r0)(c) >= v
+      })
+      .getOrElse(0) - r).abs;
+    val rd = ((r + 1 until w)
+      .find(r0 => {
+        a(r0)(c) >= v
+      })
+      .getOrElse(w - 1) - r).abs;
+    val cf = (((c - 1) to 0 by -1)
+      .find(c0 => {
+        a(r)(c0) >= v
+      })
+      .getOrElse(0) - c).abs;
+    val cd = ((c + 1 until h)
+      .find(c0 => {
+        a(r)(c0) >= v
+      })
+      .getOrElse(h - 1) - c).abs;
+    // println(s"($r, $c): $v, ($rf, $rd, $cf, $cd)")
+    rd * rf * cf * cd
+  }
+
+  def p2(input: Array[Array[Int]]): Int = {
+    val w = input.length
+    val h = input(0).length
+    (0 until w)
+      .map(r => {
+        (0 until h).map { c =>
+          val v = input(r)(c)
+          val v0 = scenicScore(input, w, h, r, c)
+          // println(s"v: $v, r: $r, c: $c, $v0")
+          v0
+        }.max
+      })
+      .max
+  }
+
   val filename = args(0);
   val input_str = Using(Source.fromFile(filename)) { source =>
     source.getLines().toArray
   }.get
   val input: Array[Array[Int]] = parseInput(input_str)
   println(p1(input))
+  println(p2(input))
 }
