d32b3535b6bf38fa967325ed732644828fbf3e9a
Eric Ye <ericye@google.com>
2023-04-19 10:12:08 -0700

    painstaking part 2 d22 for example only

--- p22/src/main.rs
+++ p22/src/main.rs
@@ -65,14 +65,14 @@ fn get_next(map: &Vec<Vec<char>>, pos: &Position) -> Position {
     next
 }
 
-fn p1(inp: Input) -> i32 {
+fn p1(inp: &Input) -> i32 {
     let mut position = Position {
         x: 0,
         y: 0,
         facing: Facing::Right,
     };
-    let map = inp.0;
-    let moves = inp.1;
+    let map = &inp.0;
+    let moves = &inp.1;
     // find first position
     for i in 0..map[0].len() {
         if map[0][i] == '.' {
@@ -81,7 +81,7 @@ fn p1(inp: Input) -> i32 {
         }
     }
     for mov in moves {
-        match mov {
+        match *mov {
             Move::Left => {
                 position.facing = match position.facing {
                     Facing::Down => Facing::Right,
@@ -110,7 +110,7 @@ fn p1(inp: Input) -> i32 {
                 }
             }
         }
-        println!("{position:?}");
+        // println!("{position:?}");
     }
     position.score()
 }
@@ -160,13 +160,300 @@ fn parse(inp: String) -> Input {
     panic!()
 }
 
+#[derive(Debug, Clone, Copy)]
+enum CubeFace {
+    Top,
+    Front,
+    Right,
+    Back,
+    Bottom,
+    Left,
+}
+
+fn project_onto_cube(map: &Vec<Vec<char>>, old_pos: &Position) -> Position {
+    assert!(map.len() % 4 == 0);
+    assert!(map.len() % 50 != 0);
+    // Can determine solely based on previous face and facing
+    const SIDE_LENGTH: i32 = 4;
+    let (net_side_x, net_side_y) = (old_pos.x / SIDE_LENGTH, old_pos.y / SIDE_LENGTH);
+    let cube_face = match (net_side_x, net_side_y) {
+        (0, 2) => CubeFace::Top,
+        (1, 0) => CubeFace::Front,
+        (1, 1) => CubeFace::Right,
+        (1, 2) => CubeFace::Back,
+        (2, 2) => CubeFace::Bottom,
+        (2, 3) => CubeFace::Left,
+        _ => panic!("Nonmatching cubeface"),
+    };
+    let new_position = match cube_face {
+        CubeFace::Top => {
+            match old_pos.facing {
+                Facing::Left => {
+                    // Top / left => right, facing down
+                    assert!(old_pos.y == 2 * SIDE_LENGTH);
+                    Position {
+                        x: 1 * SIDE_LENGTH,
+                        y: old_pos.x + 1 * SIDE_LENGTH,
+                        facing: Facing::Down,
+                    }
+                }
+                Facing::Right => {
+                    // Top / right => left, facing left
+                    assert!(old_pos.y == 3 * SIDE_LENGTH - 1);
+                    Position {
+                        x: 2 * SIDE_LENGTH + (SIDE_LENGTH - 1 - old_pos.x),
+                        y: 4 * SIDE_LENGTH - 1,
+                        facing: Facing::Left,
+                    }
+                }
+                Facing::Up => {
+                    // Top / up => front, facing down
+                    assert!(old_pos.x == 0 * SIDE_LENGTH);
+                    Position {
+                        x: 1 * SIDE_LENGTH,
+                        y: 3 * SIDE_LENGTH - 1 - old_pos.y,
+                        facing: Facing::Down,
+                    }
+                }
+                Facing::Down => {
+                    panic!("Cannot down from top");
+                }
+            }
+        }
+        CubeFace::Front => {
+            match old_pos.facing {
+                Facing::Right => panic!("Cannot right from front"),
+                Facing::Up => {
+                    // Front / up => top, facing down
+                    assert!(old_pos.x == 1 * SIDE_LENGTH);
+                    Position {
+                        x: 0,
+                        y: 2 * SIDE_LENGTH + SIDE_LENGTH - 1 - old_pos.x,
+                        facing: Facing::Down,
+                    }
+                }
+                Facing::Down => {
+                    // Front / down => bottom, facing up
+                    assert!(old_pos.x == 2 * SIDE_LENGTH - 1);
+                    Position {
+                        x: 3 * SIDE_LENGTH - 1,
+                        y: 2 * SIDE_LENGTH + (SIDE_LENGTH - 1 - old_pos.y),
+                        facing: Facing::Up,
+                    }
+                }
+                Facing::Left => {
+                    // Front / left => left, facing up
+                    assert!(old_pos.y == 0);
+                    Position {
+                        x: 3 * SIDE_LENGTH - 1,
+                        y: 3 * SIDE_LENGTH + (SIDE_LENGTH - 1 - (old_pos.x - SIDE_LENGTH)),
+                        facing: Facing::Up,
+                    }
+                }
+            }
+        }
+        CubeFace::Right => {
+            match old_pos.facing {
+                Facing::Left => panic!("Cannot left from right"),
+                Facing::Right => panic!("Cannot right from right"),
+                Facing::Up => {
+                    // Right / up => top, facing right
+                    assert!(old_pos.x == 1 * SIDE_LENGTH);
+                    Position {
+                        x: old_pos.y - SIDE_LENGTH,
+                        y: 2 * SIDE_LENGTH,
+                        facing: Facing::Right,
+                    }
+                }
+                Facing::Down => {
+                    // Right / down => bottom, facing right
+                    assert!(old_pos.x == 2 * SIDE_LENGTH - 1);
+                    Position {
+                        x: 2 * SIDE_LENGTH + (old_pos.y - SIDE_LENGTH),
+                        y: 2 * SIDE_LENGTH,
+                        facing: Facing::Right,
+                    }
+                }
+            }
+        }
+        CubeFace::Back => {
+            match old_pos.facing {
+                Facing::Left => panic!("Cannot left from back"),
+                Facing::Down => panic!("Cannot down front back"),
+                Facing::Up => panic!("Cannot up from back"),
+                Facing::Right => {
+                    // Back / right => left, facing down
+                    assert!(old_pos.y == 3 * SIDE_LENGTH - 1);
+                    Position {
+                        x: 2 * SIDE_LENGTH,
+                        y: 3 * SIDE_LENGTH + (SIDE_LENGTH - 1 - (old_pos.x - 1 * SIDE_LENGTH)),
+                        facing: Facing::Down,
+                    }
+                }
+            }
+        }
+        CubeFace::Bottom => {
+            match old_pos.facing {
+                Facing::Up => panic!("Cannot up from bottom"),
+                Facing::Right => panic!("cannot right from bottom"),
+                Facing::Down => {
+                    // Bottom / down => front, facing up
+                    assert!(old_pos.x == 3 * SIDE_LENGTH - 1);
+                    Position {
+                        x: 2 * SIDE_LENGTH - 1,
+                        y: SIDE_LENGTH - 1 - (old_pos.y - 2 * SIDE_LENGTH),
+                        facing: Facing::Up,
+                    }
+                }
+                Facing::Left => {
+                    // Bottom / left => right, facing up
+                    assert!(old_pos.y == 2 * SIDE_LENGTH);
+                    Position {
+                        x: 2 * SIDE_LENGTH - 1,
+                        y: SIDE_LENGTH + (SIDE_LENGTH - 1 - (old_pos.x - 2 * SIDE_LENGTH)),
+                        facing: Facing::Up,
+                    }
+                }
+            }
+        }
+        CubeFace::Left => {
+            match old_pos.facing {
+                Facing::Left => panic!("Cannot left from left"),
+                Facing::Down => {
+                    // Left / down => front, facing right
+                    assert!(old_pos.x == 3 * SIDE_LENGTH - 1);
+                    Position {
+                        x: SIDE_LENGTH + (SIDE_LENGTH - 1 - old_pos.y - 3 * SIDE_LENGTH),
+                        y: 0,
+                        facing: Facing::Right,
+                    }
+                }
+                Facing::Right => {
+                    // Left / right => top, facing left
+                    assert!(old_pos.y == 4 * SIDE_LENGTH - 1);
+                    Position {
+                        x: SIDE_LENGTH - 1 - (old_pos.x - SIDE_LENGTH * 2),
+                        y: 3 * SIDE_LENGTH - 1,
+                        facing: Facing::Left,
+                    }
+                }
+                Facing::Up => {
+                    // Left / up => back, facing left
+                    assert!(old_pos.x == 2 * SIDE_LENGTH);
+                    Position {
+                        x: SIDE_LENGTH + (SIDE_LENGTH - 1 - (old_pos.y - 3 * SIDE_LENGTH)),
+                        y: 3 * SIDE_LENGTH - 1,
+                        facing: Facing::Left,
+                    }
+                }
+            }
+        }
+    };
+    assert!(get_at(map, &new_position) == '.' || get_at(map, &new_position) == '#');
+    new_position
+}
+
+// Next open OR closed
+fn get_next_cube(map: &Vec<Vec<char>>, pos: &Position) -> Position {
+    let (xdelta, ydelta) = match pos.facing {
+        Facing::Down => (1, 0),
+        Facing::Up => (-1, 0),
+        Facing::Left => (0, -1),
+        Facing::Right => (0, 1),
+    };
+    let mut next = pos.clone();
+    next.x += xdelta;
+    next.y += ydelta;
+    if next.x < 0
+        || next.x as usize >= map.len()
+        || next.y < 0
+        || next.y as usize >= map[next.x as usize].len()
+        || get_at(map, &next) == ' '
+    {
+        // Cube block moving
+        project_onto_cube(map, pos)
+    } else {
+        next
+    }
+}
+
+fn write_position(pos: &Position, dbg_map: &mut Vec<Vec<char>>) {
+    dbg_map[pos.x as usize][pos.y as usize] = match pos.facing {
+        Facing::Down => 'V',
+        Facing::Left => '<',
+        Facing::Up => '^',
+        Facing::Right => '>',
+    };
+    print_dbg_map(&dbg_map);
+    println!();
+}
+
+fn print_dbg_map(map: &Vec<Vec<char>>) {
+    for line in map {
+        println!("{}", line.iter().collect::<String>());
+    }
+}
+
+fn p2(inp: &Input) -> i32 {
+    let mut position = Position {
+        x: 0,
+        y: 0,
+        facing: Facing::Right,
+    };
+    let map = &inp.0;
+    let mut dbg_map = map.clone();
+    let moves = &inp.1;
+    // find first position
+    for i in 0..map[0].len() {
+        if map[0][i] == '.' {
+            position.y = i as i32;
+            break;
+        }
+    }
+    for mov in moves {
+        println!("{mov:?}");
+        match *mov {
+            Move::Left => {
+                position.facing = match position.facing {
+                    Facing::Down => Facing::Right,
+                    Facing::Right => Facing::Up,
+                    Facing::Up => Facing::Left,
+                    Facing::Left => Facing::Down,
+                };
+                write_position(&position, &mut dbg_map);
+            }
+            Move::Right => {
+                position.facing = match position.facing {
+                    Facing::Down => Facing::Left,
+                    Facing::Right => Facing::Down,
+                    Facing::Up => Facing::Right,
+                    Facing::Left => Facing::Up,
+                };
+                write_position(&position, &mut dbg_map);
+            }
+            Move::Forward(steps) => {
+                for _ in 0..steps {
+                    let next = get_next_cube(&map, &position);
+                    if get_at(&map, &next) == '#' {
+                        break;
+                    } else {
+                        assert!(get_at(&map, &next) == '.');
+                        position = next;
+                    }
+                    write_position(&position, &mut dbg_map);
+                }
+            }
+        }
+        println!("{position:?}");
+    }
+    position.score()
+}
+
 fn main() {
     let input_string = std::fs::read_to_string(std::env::args().nth(1).expect("Need input string"))
         .expect("Couldn't read file");
     let input = parse(input_string);
     // println!("{input:?}");
-    println!("{}", p1(input));
-    // let map = pparse(&input_string);
-    // println!("p1: {}", p1(&map));
-    // println!("p2: {}", p2(&map));
+    println!("{}", p1(&input));
+    println!("{}", p2(&input));
 }
