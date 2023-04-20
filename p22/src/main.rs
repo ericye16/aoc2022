b69f32a59c249ca6199365f479abfdafd640cfec
Eric Ye <me@ericye16.com>
2023-04-19 22:53:48 -0700

    p22 p2, example again

--- p22/src/main.rs
+++ p22/src/main.rs
@@ -1,11 +1,13 @@
+use std::default;
+
 type Input = (Vec<Vec<char>>, Vec<Move>);
 
 #[derive(Debug, Clone, Copy)]
 enum Facing {
-    Up,
-    Down,
-    Left,
-    Right,
+    Up = 0,
+    Right = 1,
+    Down = 2,
+    Left = 3,
 }
 
 #[derive(Debug, Clone, Copy)]
@@ -394,7 +396,7 @@ fn print_dbg_map(map: &Vec<Vec<char>>) {
     }
 }
 
-fn p2(inp: &Input) -> i32 {
+fn not_p2(inp: &Input) -> i32 {
     let mut position = Position {
         x: 0,
         y: 0,
@@ -447,6 +449,236 @@ fn p2(inp: &Input) -> i32 {
         println!("{position:?}");
     }
     position.score()
+}
+#[derive(Debug, Clone)]
+struct CubeMap {
+    side_length: i32,
+    map: [Vec<Vec<char>>; 6],
+    cube_mapping_info: [(i32, i32, Facing); 6],
+}
+
+impl CubeMap {
+    fn from(
+        map: &Vec<Vec<char>>,
+        side_length: i32,
+        cube_infos: [(i32, i32, Facing); 6],
+    ) -> CubeMap {
+        let mut cube_map = CubeMap {
+            side_length,
+            map: [vec![], vec![], vec![], vec![], vec![], vec![]],
+            cube_mapping_info: cube_infos,
+        };
+        for face in 0..6 {
+            for _ in 0..side_length {
+                let mut v = Vec::new();
+                for _ in 0..side_length {
+                    v.push('.');
+                }
+                cube_map.map[face].push(v);
+            }
+        }
+        for (face, (xorig, yorig, facing)) in cube_infos.iter().enumerate() {
+            for x in (xorig * side_length)..(xorig * side_length + side_length) {
+                for y in (yorig * side_length)..(yorig * side_length + side_length) {
+                    let ch = map[x as usize][y as usize];
+                    let x0 = x - xorig * side_length;
+                    let y0 = y - yorig * side_length;
+                    let (xrot, yrot) = match facing {
+                        Facing::Up => (x0, y0),
+                        Facing::Down => (side_length - 1 - x0, side_length - 1 - y0),
+                        Facing::Left => (y0, side_length - 1 - x0),
+                        Facing::Right => (side_length - 1 - y0, x0),
+                    };
+                    cube_map.map[face][xrot as usize][yrot as usize] = ch;
+                }
+            }
+        }
+
+        cube_map
+    }
+
+    fn get_at(&self, coords: &CubeCoords) -> char {
+        self.map[coords.face as usize][coords.x as usize][coords.y as usize]
+    }
+
+    fn reproject(&self, coords: &CubeCoords) -> Position {
+        let (face_x, face_y, face_facing) = self.cube_mapping_info[coords.face as usize];
+        let (nx, ny) = match face_facing {
+            Facing::Up => (coords.x, coords.y),
+            Facing::Down => (
+                self.side_length - 1 - coords.x,
+                self.side_length - 1 - coords.y,
+            ),
+            Facing::Left => (self.side_length - 1 - coords.y, coords.x),
+            Facing::Right => (coords.y, self.side_length - 1 - coords.x),
+        };
+        let (ox, oy) = (face_x * self.side_length, face_y * self.side_length);
+        let new_facing = match (coords.facing as i8 + face_facing as i8).rem_euclid(4) {
+            0 => Facing::Up,
+            1 => Facing::Right,
+            2 => Facing::Down,
+            3 => Facing::Left,
+            _ => panic!(),
+        };
+        Position {
+            x: nx + ox,
+            y: ny + oy,
+            facing: new_facing,
+        }
+    }
+}
+
+#[derive(Debug, Clone, Copy)]
+struct CubeCoords {
+    x: i32,
+    y: i32,
+    facing: Facing,
+    face: CubeFace,
+}
+
+impl CubeCoords {
+    fn move_forward_one(&self, map: &CubeMap) -> CubeCoords {
+        let side_length = map.side_length;
+        let (xdelta, ydelta) = match self.facing {
+            Facing::Up => (-1, 0),
+            Facing::Down => (1, 0),
+            Facing::Left => (0, -1),
+            Facing::Right => (0, 1),
+        };
+        let mut next = self.clone();
+        next.x += xdelta;
+        next.y += ydelta;
+        if next.x < 0 || next.x >= side_length || next.y < 0 || next.y >= side_length {
+            next.face = match (self.face, self.facing) {
+                (CubeFace::Top, Facing::Up) => CubeFace::Right,
+                (CubeFace::Top, Facing::Down) => CubeFace::Left,
+                (CubeFace::Top, Facing::Left) => CubeFace::Back,
+                (CubeFace::Top, Facing::Right) => CubeFace::Front,
+                (CubeFace::Front, Facing::Up) => CubeFace::Top,
+                (CubeFace::Front, Facing::Down) => CubeFace::Bottom,
+                (CubeFace::Front, Facing::Left) => CubeFace::Left,
+                (CubeFace::Front, Facing::Right) => CubeFace::Right,
+                (CubeFace::Right, Facing::Up) => CubeFace::Front,
+                (CubeFace::Right, Facing::Down) => CubeFace::Back,
+                (CubeFace::Right, Facing::Left) => CubeFace::Bottom,
+                (CubeFace::Right, Facing::Right) => CubeFace::Top,
+                (CubeFace::Back, Facing::Up) => CubeFace::Left,
+                (CubeFace::Back, Facing::Down) => CubeFace::Right,
+                (CubeFace::Back, Facing::Left) => CubeFace::Top,
+                (CubeFace::Back, Facing::Right) => CubeFace::Bottom,
+                (CubeFace::Bottom, Facing::Up) => CubeFace::Back,
+                (CubeFace::Bottom, Facing::Down) => CubeFace::Front,
+                (CubeFace::Bottom, Facing::Left) => CubeFace::Right,
+                (CubeFace::Bottom, Facing::Right) => CubeFace::Left,
+                (CubeFace::Left, Facing::Up) => CubeFace::Bottom,
+                (CubeFace::Left, Facing::Down) => CubeFace::Top,
+                (CubeFace::Left, Facing::Left) => CubeFace::Front,
+                (CubeFace::Left, Facing::Right) => CubeFace::Back,
+            };
+            next.facing = match self.facing {
+                Facing::Up => Facing::Left,
+                Facing::Down => Facing::Up,
+                Facing::Left => Facing::Right,
+                Facing::Right => Facing::Down,
+            };
+            let along_side = match self.facing {
+                Facing::Up => self.y,
+                Facing::Down => side_length - 1 - self.y,
+                Facing::Left => side_length - 1 - self.x,
+                Facing::Right => self.x,
+            };
+            (next.x, next.y) = match next.facing {
+                Facing::Up => (side_length - 1, along_side),
+                Facing::Down => (0, side_length - 1 - along_side),
+                Facing::Left => (side_length - 1 - along_side, side_length - 1),
+                Facing::Right => (along_side, 0),
+            };
+        }
+        next
+    }
+
+    fn make_move(&mut self, mov: &Move, map: &CubeMap) {
+        match mov {
+            Move::Left => {
+                self.facing = match self.facing {
+                    Facing::Up => Facing::Left,
+                    Facing::Down => Facing::Right,
+                    Facing::Left => Facing::Down,
+                    Facing::Right => Facing::Up,
+                }
+            }
+            Move::Right => {
+                self.facing = match self.facing {
+                    Facing::Up => Facing::Right,
+                    Facing::Down => Facing::Left,
+                    Facing::Left => Facing::Up,
+                    Facing::Right => Facing::Down,
+                }
+            }
+            Move::Forward(iters) => {
+                for _ in 0..*iters {
+                    let next = self.move_forward_one(map);
+                    if map.get_at(&next) == '#' {
+                        break;
+                    } else {
+                        *self = next;
+                    }
+                }
+            }
+        }
+    }
+
+    fn display(&self, map: &CubeMap) {
+        let face = &map.map[self.face as usize];
+        println!("Face: {:?}", self.face);
+        for x in 0..map.side_length {
+            let mut s = String::new();
+            for y in 0..map.side_length {
+                s.push(if x == self.x && y == self.y {
+                    match self.facing {
+                        Facing::Up => '^',
+                        Facing::Down => 'v',
+                        Facing::Left => '<',
+                        Facing::Right => '>',
+                    }
+                } else {
+                    face[x as usize][y as usize]
+                })
+            }
+            println!("{}", s);
+        }
+    }
+}
+
+fn p2(input: &Input) -> i64 {
+    let original_map = &input.0;
+    let moves = &input.1;
+    let cube_map = CubeMap::from(
+        original_map,
+        4,
+        [
+            /* top */ (0, 2, Facing::Left),
+            /* front */ (1, 0, Facing::Up),
+            /* right */ (1, 1, Facing::Left),
+            /* back */ (1, 2, Facing::Right),
+            /* bottom */ (2, 2, Facing::Up),
+            /* left */ (2, 3, Facing::Left),
+        ],
+    );
+    let mut position = CubeCoords {
+        x: 0,
+        y: 3,
+        facing: Facing::Down,
+        face: CubeFace::Top,
+    };
+    for mov in moves {
+        println!("{:?}", mov);
+        position.make_move(mov, &cube_map);
+        position.display(&cube_map);
+    }
+    let projected_position = cube_map.reproject(&position);
+    dbg!(projected_position);
+    projected_position.score().into()
 }
 
 fn main() {
